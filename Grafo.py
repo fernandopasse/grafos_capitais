86c2fc6192105baf2d8261cf4893352510988f17
fernando.passe@ufv.br <fernandopasse@outlook.com>
2015-11-12 15:16:11 -0200

    Classe Grafo

---
+++ Grafo.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from graphviz import Graph
+import networkx as bbt_grafo
+import xlrd as excel
+
+
+class Grafo:
+    def __init__(self):
+        self.meuGrafo = bbt_grafo.Graph()
+
+    def __str__(self):
+        A = Graph()
+        for aresta_A, aresta_B, dados_aresta in self.meuGrafo.edges_iter(data=True):
+            peso_aresta = dict(('label', str(peso)) for _, peso in dados_aresta.items())
+            A.edge(aresta_A.encode("utf8"), aresta_B.encode("utf8"), **peso_aresta)
+        return A.source
+
+    def adicionar_vertice(self, vertice):
+        self.meuGrafo.add_node(vertice)
+
+    def adicionar_aresta(self, aresta_A, aresta_B, peso):
+        self.meuGrafo.add_edge(aresta_A, aresta_B, weight=peso)
+
+    def ler_excel(self, arquivo):
+        planilha = excel.open_workbook(arquivo, formatting_info=True)
+        primeiraTabela = planilha.sheet_by_index(0)
+        for i in range(1, primeiraTabela.nrows):
+            for j in range(1, primeiraTabela.ncols):
+                if primeiraTabela.cell(i, j).value != '':
+                    self.adicionar_aresta(primeiraTabela.cell(i, 0).value, primeiraTabela.cell(0, j).value,
+                                          primeiraTabela.cell(i, j).value)
+        return True
+
+    def renderiza_grafo(self, lugar_para_gerar, nome_grafo):
+        A = Graph(comment='The Round Table', filename=(lugar_para_gerar + 'Grafo.gv'), engine='dot')
+        for aresta_A, aresta_B, dados_aresta in self.meuGrafo.edges_iter(data=True):
+            peso_aresta = dict(('label', str(peso)) for _, peso in dados_aresta.items())
+            A.edge(aresta_A, aresta_B, **peso_aresta)
+        A.view()
--- main.py
+++ main.py
@@ -1,54 +1,8 @@
 # -*- coding: utf-8 -*-
-import xlrd
-from graphviz import Graph
-import networkx as nx
-
-def geraGrafo(N):
-    A = Graph('G', filename='Grafo.gv', engine='dot')
-    # add nodes
-    # loop over edges
-    """
-    if N.is_multigraph():
-        for u, v, key, edgedata in N.edges_iter(data=True, keys=True):
-            str_edgedata = dict(('label', str(v)) for k, v in edgedata.items())
-            A.edge(u, v, key=str(key), **str_edgedata)
-    else:
-    """
-    for u, v, edgedata in N.edges_iter(data=True):
-        str_edgedata = dict(('label', str(v)) for k, v in edgedata.items())
-        A.edge(u, v, **str_edgedata)
-    A.view()
-"""
-Função <abrir_arquivo>: faz a leitura do arquivo xls com dados
-"""
-def abrirArquivo(local):
-    planilha = xlrd.open_workbook(local, formatting_info=True)
-    primeira_tabela = planilha.sheet_by_index(0)
-    numero_colunas = primeira_tabela.ncols
-    numero_linhas = primeira_tabela.nrows
-    G = nx.Graph()  # Cria um novo grafo
-    for cel_linha in range(1, numero_linhas):
-        for cel_coluna in range(1, numero_colunas):
-            cel_nome_linha = primeira_tabela.cell(cel_linha, 0)
-            cel_nome_coluna = primeira_tabela.cell(0, cel_coluna)
-            cel_valor = primeira_tabela.cell(cel_linha, cel_coluna)
-            cel_id = primeira_tabela.cell_xf_index(cel_linha, cel_coluna)
-            cel_obj = planilha.xf_list[cel_id]
-            cel_fundo = cel_obj.background.pattern_colour_index
-            if cel_valor.value != '':
-                # print('(%s, %s, %s, %s)' % (cel_nome_linha.value, cel_nome_coluna.value, cel_valor.value, cel_fundo))
-                G.add_edge(cel_nome_linha.value, cel_nome_coluna.value,
-                           weight=cel_valor.value)  # adiciona arestas ao grafo
-    return G
-
-
-def algoritmo_prim(grafo):
-    return nx.minimum_spanning_tree(grafo)
-
+from Grafo import Grafo
 
 if __name__ == "__main__":
     local = "dados.xls"
-    grafo = abrirArquivo(local)
-    arvGeradoraMinima = algoritmo_prim(grafo)
-    print(sorted(arvGeradoraMinima.edges(data=True)))
-    geraGrafo(arvGeradoraMinima)
+    grafo = Grafo()
+    grafo.ler_excel(local)
+    grafo.renderiza_grafo("","Teste")
